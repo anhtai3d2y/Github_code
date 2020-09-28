11ecdb884de536078ea5f6ff8471c869078178fa
anhtai3d2y <anhtai3d2y@gmail.com>
2020-09-28 16:40:52 +0700

    Add all file

---
+++ HTML/Calculator/script.js
@@ -0,0 +1,40 @@
+class Calculator {
+	constructor(previousOperandTextElement, currentOperandTextElement){
+		this.previousOperandTextElement = previousOperandTextElement
+		this.currentOperandTextElement = currentOperandTextElement
+	}
+
+	clear(){
+		this.currentOperand = ''
+	}
+
+	delete(){
+
+	}
+
+	appendNumber(number){
+
+	}
+	chooseOperation(operation){
+
+	}
+	computer(){
+
+	}
+	updateDisplay(){
+
+	}
+}
+
+
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButtons = document.querySelectorAll('[data-equals]')
+const deleteButtons = document.querySelectorAll('[data-delete]')
+const allClearButtons = document.querySelectorAll('[data-all-clear]')
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
